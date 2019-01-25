3b8e23e35bda585d14f7b0eab991c53cdd370ef1
Doris Warren <doriswarren11@gmail.com>
2019-01-25 12:47:55 -0800

    turn input into string checking for zeros

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 function findOnes(input){
-  var result = input.toString().indexOf(1);
-  if (result === -1) {
-    return false;
-  } else {
-    return true;
-  }
+    var result = input.toString().indexOf(1);
+    if (result === -1) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
+function findZeros(input) {
+    var result = input.toString().indexOf(0);
+    if (result === -1) {
+        return false;
+    } else {
+        return true;
+    }
 }
