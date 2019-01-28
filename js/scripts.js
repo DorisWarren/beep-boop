78da03e215c52e4378a3429d4db5b945e427aca5
Student <example@example.com>
2019-01-28 15:04:05 -0800

    show results of 1,2,3 - fixed functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,52 @@
+function findOnes(inputNumber){
+  var result = inputNumber.toString().indexOf(1);
+  if (result === -1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function findTwos(inputNumber){
+  var result = inputNumber.toString().indexOf(0);
+  if (result === -1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function findThree(inputNumber){
+  var result = inputNumber % 3;
+  if (result === 0 && inputNumber !=0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function convertInput(inputNumber){
+  var numberArray = [];
+  for (var i = 0; i <= inputNumber; i++) {
+    if (findThree(i)) {
+      numberArray.push("I'm sorry, Dave! I am afraid i cant do that.");
+    } else if ( findOnes(i)) {
+      numberArray.push("Boop!");
+    } else if (findTwos(i)) {
+      numberArray.push("Beep!");
+    } else {
+      numberArray.push(i);
+    }
+  }
+  return numberArray;
+}
 
 $(document).ready(function(){
-
-  $("#intakeForm").on("submit", function(event){
+  $("#intakeForm").submit(function(event){
     event.preventDefault();
     var inputNumber = $("#inputNumber").val();
     var resultArray = convertInput(inputNumber);
-    var result = "";
-    for (var i = 0; i < resultArray.length; i++) {
-        result += resultArray[i] + " ";
-    }
-    $("#result").text(result);
+
+    $("#result").text(resultArray);
   });
 });
-
-function findOnes(input){
-    var result = input.toString().indexOf(1);
-    if (result === -1) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function findTwos(input){
-    var result = input.toString().indexOf(0);
-    if (result === -1) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function divByThree(input){
-    var result = input % 3;
-    if (result === 0 && input !=0){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function convertInput(input){
-    var numberArray = [];
-    for (var i = 0; i <= input; i++) {
-      if (divByThree(i) === true) {
-        numberArray.push("I'm sorry, Dave! I am afraid i cant do that.");
-      } else if ( findOnes(i) === true) {
-        numberArray.push("Boop!");
-      } else if (findTwos(i) === true) {
-        numberArray.push("Beep!");
-      } else {
-        numberArray.push(i);
-      }
-    }
-    return numberArray;
-}
